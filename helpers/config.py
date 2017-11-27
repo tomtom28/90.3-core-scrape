f74b8c3fb8da21aecadb795d9f73e3d53cd5370a
Tommy Thompson <tomthompson828@gmail.com>
2017-11-26 21:14:46 -0500

    test

--- helpers/config.py
+++
@@ -1,6 +0,0 @@
-# https://beta.developer.spotify.com/dashboard/applications
-
-CLIENT_ID = "YOUR KEY ID HERE"
-CLIENT_SECRET = "YOUR SECRET HERE"
-SPOTIFY_USER_ID = "SPOTIFY USER ID HERE"
-SPOTIFY_PLAYLIST_ID = "SPOTIFY PLAYLIST ID HERE"
