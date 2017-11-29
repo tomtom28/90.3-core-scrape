86df105488f475f2a18497b20169b0334f76913b
tomtom28 <tomthompson828@gmail.com>
2017-11-29 10:21:22 -0500

    memo to print to user on startup

--- __init__.py
+++ __init__.py
@@ -4,6 +4,7 @@ import time
 # Fire off scraping app every 5 minutes
 from app import scrape_music
 schedule.every(5).minutes.do(scrape_music)
+print "Scraped Scheduled . . ."
 
 while True:
     schedule.run_pending()
